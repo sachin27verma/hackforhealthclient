8d8fa998a78338648875d5ed20b146736681b98d
sachin27verma <97831442+sachin27verma@users.noreply.github.com>
2024-01-19 21:39:25 +0530

    change callback

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -50,7 +50,14 @@ import GoogleProvider from "next-auth/providers/google";
           
           // Do different verification for other providers that don't have `email_verified`
         },
-      }
+      },
+      async redirect({ url, baseUrl }) {
+        // Allows relative callback URLs
+        if (url.startsWith("/")) return `${baseUrl}${url}`
+        // Allows callback URLs on the same origin
+        else if (new URL(url).origin === baseUrl) return url
+        return baseUrl
+      },
 })
 
  export  { handler as GET, handler as POST }
--- src/middleware.js
+++ src/middleware.js
@@ -3,3 +3,11 @@
 export {default} from "next-auth/middleware"
 
 export const config ={matcher:['/wellnesspromotation','/healthequity','/Mentalhealthsupport','/remotepatient']};
+
+// import { withAuth } from "next-auth/middleware"
+
+// export default withAuth({
+//   callbacks: {
+//     authorized: ({ token }) => token?.userRole === "admin",
+//   },
+// })
